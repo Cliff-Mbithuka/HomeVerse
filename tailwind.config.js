f4f441f1cc63996e4d5c0d5f1c383480871e61a7
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-10-25 08:23:32 +0300

    Style header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: [ './src/**/*.{html,js,ts,jsx,tsx}',
     './public/index.html',],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'body': ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
